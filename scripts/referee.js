c7524ef88e309153ac5edd021dfdc20bc815b4fd
begeeben <begeeben@gmail.com>
2012-10-18 16:41:39 +0800

    Testing YQL. Split referee.js into referee.main.js.

--- scripts/referee.js
+++ scripts/referee.js
@@ -651,56 +651,56 @@ var Referee = {
     }
 };
 
-$(document).ready(function () {
-    $('#login_dialog').dialog({
-        autoOpen: true,
-        draggable: false,
-        modal: true,
-        title: 'Connect to XMPP',
-        buttons: {
-            "Connect": function () {
-                $(document).trigger('connect', {
-                    jid: $('#jid').val().toLowerCase(),
-                    password: $('#password').val()
-                });
+//$(document).ready(function () {
+//    $('#login_dialog').dialog({
+//        autoOpen: true,
+//        draggable: false,
+//        modal: true,
+//        title: 'Connect to XMPP',
+//        buttons: {
+//            "Connect": function () {
+//                $(document).trigger('connect', {
+//                    jid: $('#jid').val().toLowerCase(),
+//                    password: $('#password').val()
+//                });
 
-                $('#password').val('');
-                $(this).dialog('close');
-            }
-        }
-    });
-});
+//                $('#password').val('');
+//                $(this).dialog('close');
+//            }
+//        }
+//    });
+//});
 
-$(document).bind('connect', function (ev, data) {
-    var conn = new Strophe.Connection(
-        "http://bosh.metajack.im:5280/xmpp-httpbind");
+//$(document).bind('connect', function (ev, data) {
+//    var conn = new Strophe.Connection(
+//        "http://bosh.metajack.im:5280/xmpp-httpbind");
 
-    // debug
-    conn.rawInput = function (data) {
-        console.log(data);
-    };
-    conn.rawOutput = function (data) {
-        console.log(data);
-    };
+//    // debug
+//    conn.rawInput = function (data) {
+//        console.log(data);
+//    };
+//    conn.rawOutput = function (data) {
+//        console.log(data);
+//    };
 
-    conn.connect(data.jid, data.password, function (status) {
-        if (status === Strophe.Status.CONNECTED) {
-            $(document).trigger('connected');
-        } else if (status === Strophe.Status.DISCONNECTED) {
-            $(document).trigger('disconnected');
-        }
-    });
+//    conn.connect(data.jid, data.password, function (status) {
+//        if (status === Strophe.Status.CONNECTED) {
+//            $(document).trigger('connected');
+//        } else if (status === Strophe.Status.DISCONNECTED) {
+//            $(document).trigger('disconnected');
+//        }
+//    });
 
-    Referee.connection = conn;
-});
+//    Referee.connection = conn;
+//});
 
-$(document).bind('connected', function () {
-    var conn = Referee.connection;
+//$(document).bind('connected', function () {
+//    var conn = Referee.connection;
 
-    $('#log').prepend("<p>Connected as " + conn.jid + "</p>");
+//    $('#log').prepend("<p>Connected as " + conn.jid + "</p>");
 
-    conn.addHandler(Referee.on_presence, null, "presence");
-    conn.addHandler(Referee.on_iq, null, "iq");
+//    conn.addHandler(Referee.on_presence, null, "presence");
+//    conn.addHandler(Referee.on_iq, null, "iq");
 
-    conn.send($pres());
-});
+//    conn.send($pres());
+//});
