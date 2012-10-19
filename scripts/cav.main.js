0537b39296dbd0b6acebf4b0eeebae29cee44caf
Eric Tsao <eric.tsao.jr@gmail.com>
2012-10-20 06:11:32 +0800

    cav.html 1. 因應將建立Poker的動作拉到jquery.cavPoker, 修改Template cav.game.js 1. 將發牌中的建立Poker動作改為呼叫$.cavPoker 2. 將發牌動作改為呼叫$.cavPokerMoveTo cav.main.js 1. 修改測試連線function中的code jquery.cavPoker.js 1. $.cavPoker 2. $.cavMoveTo 3. Poker Click Event 4. Poker Drag Event

--- scripts/cav.main.js
+++ scripts/cav.main.js
@@ -45,7 +45,7 @@
         Cav.GameController.Switch({
             FunctionName: 'GameStarted',
             Turn: 1,
-            PokerCards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,0],
+            PokerCards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
             PicMapping: ["images/TmpGirl/Joker.jpg", "images/TmpGirl/Beauty01.jpg", "images/TmpGirl/Beauty02.jpg", "images/TmpGirl/Beauty03.jpg", "images/TmpGirl/Beauty04.jpg", "images/TmpGirl/Beauty05.jpg", "images/TmpGirl/Beauty06.jpg", "images/TmpGirl/Beauty07.jpg", "images/TmpGirl/Beauty08.jpg", "images/TmpGirl/Beauty09.jpg", "images/TmpGirl/Beauty10.jpg", "images/TmpGirl/Beauty11.jpg", "images/TmpGirl/Beauty12.jpg", "images/TmpGirl/Beauty13.jpg"],
             Index1: 3,
             Index2: 2
--- scripts/jquery.cavPoker.js
+++ scripts/jquery.cavPoker.js
@@ -1,5 +1,143 @@
 ﻿(function ($) {
-	$.createPoker = function (t, p) { };
+	$.createPoker = function (t, p) {
+		if (t.cav) return false; //return if already exist
+
+		p = $.extend({ //apply default properties
+			Id: -1,
+			No: -1,
+			Suit: -1,
+			ImgUrl:"",
+			handCard: true,
+			pokerTemplate: "",
+			zIndex: -1,
+			left: 0,
+			top:0
+		}, p);
+
+		var pokerHtml = Mustache.to_html(p.pokerTemplate, p).replace(/^\s*/mg, '');
+		$(t).append(pokerHtml)
+			.attr("id", p.Id)
+			.addClass("Poker")
+			.addClass(p.handCard ? "HandCard" : "OppHandCard")
+			.css("z-index", p.zIndex)
+			.css("left", p.left)
+			.css("top", p.top);
+
+		var c = {
+			moveTo: function (left, top, speed) {
+				$(t).animate({ left: left, top: top }, speed);
+			},
+			moveUpFlag: false,
+			moveUp: function () {
+				$(t).animate({ top: "-=92", "z-index": "+=100", width: "+=48", height: "+=72" }, 100)
+				.find(".Beauty").animate({ width: "+=48", height: "+=72" }, 100);
+			},
+			moveDown: function () {
+				$(t).animate({ top: "+=92", "z-index": "-=100", width: "-=48", height: "-=72" }, 100)
+				.find(".Beauty").animate({ width: "-=48", height: "-=72" }, 100);
+			},
+
+			dragStart: function (e) {
+				console.log("dragStart");
+				document.oPosition = $(t).offset();
+				document.oPosition.right = document.oPosition.left + $(t).width();
+				document.oPosition.bottom = document.oPosition.top + $(t).height();
+				document.dragPoker = t;
+				document.dragPokerIndex = $(".HandCard").index(t);
+				document.pokerCopy = $(t).clone().removeClass("HandCard");
+				$(document.pokerCopy).css({
+					position: 'absolute',
+					float: 'left',
+					display: 'none'
+				});
+				$('body').append(document.pokerCopy);
+				$('body').noSelect();
+
+				$(document).bind("mousemove", function (e) {
+					c.dragMove(e);
+				});
+			},
+			dragMove: function (e) {
+				if (document.pokerCopy) {
+					console.log("dragMove");
+					if (e.pageX > document.oPosition.right || e.pageX < document.oPosition.left || e.pageY > document.oPosition.bottom || e.pageY < document.oPosition.top) {
+						$('body').css('cursor', 'move');
+					} else {
+						$('body').css('cursor', 'pointer');
+					}
+					$(document.pokerCopy).css({
+						top: e.pageY + 15,
+						left: e.pageX + 15,
+						display: 'block'
+					});
+					$(document.dragPoker).hide();
+				}
+			},
+			dragEnd: function () {
+				if (document.pokerCopy) {
+					$(document.pokerCopy).remove();
+					$(document.dragPoker).show();
+					if (document.dragPokerTargetIndex != null) {
+						console.log(document.dragPokerTargetIndex);
+						console.log(document.dragPokerIndex);
+						console.log("dragEnd");
+
+						var tp = $(".HandCard").eq(document.dragPokerTargetIndex);
+						var tSet = tp.offset();
+						var tz = tp.css("z-index");
+
+						var op = $(".HandCard").eq(document.dragPokerIndex);
+						var oSet = op.offset();
+						var oz = op.css("z-index");
+
+						tp.animate({ top: oSet.top - 5, left: oSet.left - 5, "z-index": oz });
+						op.animate({ top: tSet.top - 5, left: tSet.left - 5, "z-index": tz });
+					}
+					document.dragPoker = null;
+					document.oPosition = null;
+					document.dragPokerIndex = null;
+					document.dragPokerTargetIndex = null;
+					document.pokerCopy = null;
+				}
+				$('body').css('cursor', 'default');
+				$('body').noSelect(false);
+
+				$(document).unbind("mousemove");
+			},
+		};
+
+		$(t).click(function () {
+			if (!c.moveUpFlag) {
+				c.moveUp();
+				c.moveUpFlag = true;
+			} else {
+				c.moveDown();
+				c.moveUpFlag = false;
+			}
+		}).mousedown(function (e) {
+			if (!$(t).hasClass("HandCard")) return;
+
+			c.dragStart(e);
+		}).mouseenter(function () {
+			if (document.pokerCopy) {
+				var n = $(".HandCard").index(this);
+				if (n == document.dragPokerIndex) {
+					return false;
+				}
+
+				document.dragPokerTargetIndex = n;
+			}
+		});
+
+		$(document).mouseup(function () { }, function (e) {
+			c.dragEnd();
+		});
+
+		t.p = p;
+		t.cav = c;
+
+		return t;
+	};
 
 	var docloaded = false;
 	$(document).ready(function () {
@@ -9,14 +147,43 @@
 	$.fn.cavPoker = function (p) {
 		return this.each(function () {
 			if (!docloaded) {
-				$(this).hide();
 				var t = this;
 				$(document).ready(function () {
-					$.addFlex(t, p);
+					$.createPoker(t, p);
 				});
 			} else {
-				$.addFlex(this, p);
+				$.createPoker(this, p);
 			}
 		});
+	};
+	$.fn.cavPokerMoveTo = function (left, top, speed) {
+		return this.each(function () {
+			if (this.cav) this.cav.moveTo(left, top, speed);
+		});
+	};
+
+	$.fn.noSelect = function (p) { //no select plugin by me :-)
+		var prevent = (p == null) ? true : p;
+		if (prevent) {
+			return this.each(function () {
+				if ($.browser.msie || $.browser.safari) $(this).bind('selectstart', function () {
+					return false;
+				});
+				else if ($.browser.mozilla) {
+					$(this).css('MozUserSelect', 'none');
+					$('body').trigger('focus');
+				} else if ($.browser.opera) $(this).bind('mousedown', function () {
+					return false;
+				});
+				else $(this).attr('unselectable', 'on');
+			});
+		} else {
+			return this.each(function () {
+				if ($.browser.msie || $.browser.safari) $(this).unbind('selectstart');
+				else if ($.browser.mozilla) $(this).css('MozUserSelect', 'inherit');
+				else if ($.browser.opera) $(this).unbind('mousedown');
+				else $(this).removeAttr('unselectable', 'on');
+			});
+		}
 	};
 })(jQuery);
