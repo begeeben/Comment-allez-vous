a416521554c09a5b95d5d31ad442af403e7d5e3e
begeeben <begeeben@gmail.com>
2012-10-19 17:29:03 +0800

    Added <div id='page-container'></div>. Referred the script to current jQuery version. Commented out test code. Separated object literal and execution logic.

---
+++ scripts/cav.main.js
@@ -0,0 +1,172 @@
+﻿$(document).ready(function () {
+    $('#login_dialog').dialog({
+        autoOpen: true,
+        draggable: false,
+        modal: true,
+        title: 'Connect to XMPP',
+        buttons: {
+            "Connect": function () {
+                $(document).trigger('connect', {
+                    jid: $('#jid').val().toLowerCase(),
+                    password: $('#password').val(),
+                    referee: $('#referee').val().toLowerCase()
+                });
+
+                $('#password').val('');
+                $(this).dialog('close');
+            }
+        }
+    });
+
+    $('#disconnect').click(function () {
+        $(this).attr('disabled', 'disabled');
+
+        Cav.connection.disconnect();
+    });
+
+    $('#wait').click(function () {
+        $(this).attr('disabled', 'disabled');
+
+        Cav.connection.sendIQ(
+            $iq({ to: Cav.referee, type: "set" })
+                .c("waiting", { xmlns: Cav.NS_CAV }));
+    });
+
+    // test CavMsg conversion
+    //$('#sendCavMsg').click(function () {
+    //    Cav.submitMovement({
+    //        functionName: 'test',
+    //        turn: 1,
+    //        pokerCards: [1, 2],
+    //        picMapping: ['http://google.com', 'http://yahoo.com'],
+    //        index1: 3,
+    //        index2: 2
+    //    });
+    //});
+    //$('#getCavMsg').click(function () {
+    //    var testCav = Cav.convertGameMessage("<message to='elizabeth@longbourn.lit/sitting_room' from='toetem-789@games.pemberley.lit/referee' type='groupchat'><move xmlns='https://github.com/begeeben/Comment-allez-vous' functionName='test' turn=1 pokerCards='1 2' picMapping='http://google.com http://yahoo.com' index1=3 index2=2/></message>");
+    //    return true;
+    //});
+
+    $('input.stop_button').live('click', function () {
+        Cav.connection.sendIQ(
+            $iq({ to: Cav.referee, type: "set" })
+                .c('stop-waiting', { xmlns: Cav.NS_CAV }),
+            function () {
+                $('#wait').removeAttr('disabled');
+            });
+    });
+
+    $('input.start_button').live('click', function () {
+        Cav.connection.sendIQ(
+            $iq({ to: Cav.referee, type: "set" })
+                .c('start', {
+                    xmlns: Cav.NS_CAV,
+                    "with": $(this).parent().prev().text()
+                }));
+    });
+
+    $('input.watch_button').live('click', function () {
+        // join the game room
+        Cav.game = $(this).parent().prev().text();
+        Cav.watching = true;
+
+        $('#browser').hide();
+        $('#game').show();
+        Cav.draw_board();
+        $('#board-status').html('');
+
+        Cav.connection.send(
+            $pres({ to: Cav.game + '/' + Cav.connection.jid }));
+    });
+
+    $('#input').keypress(function (ev) {
+        if (ev.which === 13) {
+            ev.preventDefault();
+
+            var input = $(this).val();
+            $(this).val('');
+
+            Cav.connection.send(
+                $msg({ to: Cav.game, type: 'groupchat' })
+                    .c('body').t(input));
+        }
+    });
+
+    $('#resign').click(function () {
+        Cav.connection.sendIQ(
+            $iq({ to: Cav.referee, type: 'set' })
+                .c('resign', { xmlns: Cav.NS_CAV }));
+    });
+
+    $('#leave').click(function () {
+        Cav.connection.send(
+            $pres({
+                to: Cav.game + '/' + Cav.connection.jid,
+                type: 'unavailable'
+            }));
+        $('#game').hide();
+        $('#browser').show();
+    });
+
+    //    $('#board').click(function (ev) {
+    //        if (Cav.turn && Cav.turn === Cav.my_side) {
+    //            var pos = $(this).position();
+    //            var x = Math.floor((ev.pageX - pos.left) / 100);
+    //            var y = Math.floor((ev.pageY - pos.top) / 100);
+
+    //            Cav.connection.sendIQ(
+    //                $iq({ to: Cav.referee, type: 'set' })
+    //                    .c('move', { xmlns: Cav.NS_CAV,
+    //                        col: ['a', 'b', 'c'][x],
+    //                        row: y + 1
+    //                    }));
+    //        }
+    //    });
+});
+
+$(document).bind('connect', function (ev, data) {
+    var conn = new Strophe.Connection(
+        "http://bosh.metajack.im:5280/xmpp-httpbind");
+
+    // debug
+    conn.rawInput = function (data) {
+        console.log(data);
+    };
+    conn.rawOutput = function (data) {
+        console.log(data);
+    };
+
+    conn.connect(data.jid, data.password, function (status) {
+        if (status === Strophe.Status.CONNECTED) {
+            $(document).trigger('connected');
+        } else if (status === Strophe.Status.DISCONNECTED) {
+            $(document).trigger('disconnected');
+        }
+    });
+
+    Cav.connection = conn;
+    Cav.referee = data.referee;
+});
+
+$(document).bind('connected', function () {
+    $('#disconnect').removeAttr('disabled');
+    $('#wait').removeAttr('disabled');
+
+    Cav.connection.addHandler(Cav.on_message, null, "message");
+
+    // tell the referee we're online
+    Cav.connection.send(
+        $pres({ to: Cav.referee })
+            .c('register', { xmlns: Cav.NS_CAV }));
+});
+
+$(document).bind('disconnected', function () {
+    Cav.referee = null;
+    Cav.connection = null;
+
+    $('#waiting tbody').empty();
+    $('#games tbody').empty();
+
+    $('#login_dialog').dialog('open');
+});
